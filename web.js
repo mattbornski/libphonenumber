feb074a31214fae558bb284d9a27288482acf586
guillaume <guillaume@justin.tv>
2012-01-27 17:29:39 -0800

    Return more info on the number

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ goog.require('goog.array');
 goog.require('goog.proto2.PbLiteSerializer');
 goog.require('goog.string');
 goog.require('goog.string.StringBuffer');
+goog.require('goog.json');
 
 goog.loadScript('closure/goog/i18n/phonenumbers/phonemetadata.pb.js');
 goog.loadScript('closure/goog/i18n/phonenumbers/phonenumber.pb.js');
@@ -14,6 +15,7 @@ goog.loadScript('closure/goog/i18n/phonenumbers/phonenumberutil.js');
 
 var phoneUtil = goog.global.i18n.phonenumbers.PhoneNumberUtil.getInstance();
 var PNF = goog.global.i18n.phonenumbers.PhoneNumberFormat;
+var PNT = goog.global.i18n.phonenumbers.PhoneNumberType;
 
 var express = require('express');
 var app = express.createServer(express.logger());
@@ -23,9 +25,50 @@ app.get('/', function(req, response) {
   var country_code = req.param('country_code');
 
   var number = phoneUtil.parseAndKeepRawInput(number, country_code);
-  var inumber = phoneUtil.format(number, PNF.E164)
+  var inumber = phoneUtil.format(number, PNF.E164);
+  var national = phoneUtil.format(number, PNF.NATIONAL);
   
-  response.send(inumber);
+  var is_valid = phoneUtil.isValidNumber(number);
+  var type = 'UNKNOWN';
+  
+  switch (phoneUtil.getNumberType(number)) {
+    case PNT.FIXED_LINE:
+      type = 'FIXED_LINE';
+      break;
+    case PNT.MOBILE:
+      type ='MOBILE';
+      break;
+    case PNT.FIXED_LINE_OR_MOBILE:
+      type ='FIXED_LINE_OR_MOBILE';
+      break;
+    case PNT.TOLL_FREE:
+      type ='TOLL_FREE';
+      break;
+    case PNT.PREMIUM_RATE:
+      type ='PREMIUM_RATE';
+      break;
+    case PNT.SHARED_COST:
+      type ='SHARED_COST';
+      break;
+    case PNT.VOIP:
+      type ='VOIP';
+      break;
+    case PNT.PERSONAL_NUMBER:
+      type ='PERSONAL_NUMBER';
+      break;
+    case PNT.PAGER:
+      type ='PAGER';
+      break;
+    case PNT.UAN:
+      type ='UAN';
+      break;
+    case PNT.UNKNOWN:
+      type ='UNKNOWN';
+      break;
+  }
+  
+  var j = goog.json.serialize({e164: inumber, national: national, valid: is_valid, type: type});
+  response.send(j);
 });
 
 var port = process.env.PORT || 3000;
